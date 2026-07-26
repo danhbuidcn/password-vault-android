30d4878bed570fdfa1c09394de11dbad5619433c
danhbuidcn <danhbuidcn@gmail.com>
2026-07-26 23:12:15 +0700

    chore: bump version to 0.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.pwvault.app"
         minSdk = 26
         targetSdk = 37
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.3.0"
         buildConfigField("boolean", "ENABLE_SCREENSHOT_BLOCK", "$enableScreenshotBlock")
     }
 
